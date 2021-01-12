e26e35e09bece67930a47664a1157d9dfec4edcf
Akhmadeev Timur <tmakhmadeev@gmail.com>
2021-01-12 22:09:51 +0300

    Changing fragment to work with movie from Json, adding a companion object

--- app/src/main/java/ru/androidacademy/gooseeye/fragments/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/androidacademy/gooseeye/fragments/FragmentMoviesDetails.kt
@@ -2,24 +2,25 @@ package ru.androidacademy.gooseeye.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import ru.androidacademy.gooseeye.MainActivity
-import ru.androidacademy.gooseeye.models.ArtistInfo
-import ru.androidacademy.gooseeye.adapters.ArtistRecyclerAdapter
+import com.bumptech.glide.Glide
 import ru.androidacademy.gooseeye.R
+import ru.androidacademy.gooseeye.RatingBarSvg
+import ru.androidacademy.gooseeye.adapters.ArtistRecyclerAdapter
+import ru.androidacademy.gooseeye.data.Actor
+import ru.androidacademy.gooseeye.data.Movie
 
 class FragmentMoviesDetails : Fragment() {
+
+    private lateinit var movie: Movie
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,8 +30,21 @@ class FragmentMoviesDetails : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setUpRecyclerViewArtist()
+        movie = arguments?.getParcelable(MOVIE)!!
+        Glide
+            .with(view)
+            .load(movie.backdrop)
+            .into(view.findViewById(R.id.img_poster))
+        view.findViewById<TextView>(R.id.tv_pg).text =
+            "${movie.minimumAge}${view.resources.getString(R.string.pg_text)}"
+        view.findViewById<TextView>(R.id.tv_name).text = movie.title
+        view.findViewById<TextView>(R.id.tv_tag_line).text = movie.getGenres()
+        view.findViewById<RatingBarSvg>(R.id.ratingBar).rating = (movie.ratings) / 2
+        view.findViewById<TextView>(R.id.tv_review).text =
+            "${movie.numberOfRatings} ${view.resources.getString(R.string.review_text)}"
+        view.findViewById<TextView>(R.id.tv_story_line_details).text = movie.overview
         val backButton = view.findViewById<TextView>(R.id.btn_back)
+        setUpRecyclerViewArtist()
         backButton.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()?.apply {
                 remove(this@FragmentMoviesDetails)
@@ -42,26 +56,34 @@ class FragmentMoviesDetails : Fragment() {
 
     private fun setUpRecyclerViewArtist() {
         val recyclerView = view?.findViewById<RecyclerView>(R.id.rv_artist_list)
-        val layoutManager = LinearLayoutManager(requireActivity())
-        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        val divider = DividerItemDecoration(recyclerView?.context, layoutManager.orientation)
-        divider.setDrawable(ContextCompat.getDrawable(recyclerView?.context!!, R.drawable.divider_separator)!!)
-        recyclerView.addItemDecoration(divider)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = ArtistRecyclerAdapter(fillList())
+        val linearLayoutManager = LinearLayoutManager(requireActivity())
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        val divider = DividerItemDecoration(recyclerView?.context, linearLayoutManager.orientation)
+        divider.setDrawable(
+            ContextCompat.getDrawable(
+                recyclerView?.context!!,
+                R.drawable.divider_separator
+            )!!
+        )
+        recyclerView.apply {
+            addItemDecoration(divider)
+            layoutManager = linearLayoutManager
+            adapter = ArtistRecyclerAdapter(fillList())
+        }
     }
 
-    private fun fillList(): List<ArtistInfo> {
-        return mutableListOf(
-            ArtistInfo(R.drawable.robert_downey, getString(R.string.robert_downey)),
-            ArtistInfo(R.drawable.chris_evans, getString(R.string.chris_evans)),
-            ArtistInfo(R.drawable.mark_ruffalo, getString(R.string.mark_ruffalo)),
-            ArtistInfo(R.drawable.chris_hemsworth, getString(R.string.chris_hemsworth)),
-            ArtistInfo(R.drawable.scarlett_johansson, getString(R.string.scarlett_johansson)),
-            ArtistInfo(R.drawable.jeremy_renner, getString(R.string.jeremy_renner)),
-            ArtistInfo(R.drawable.don_cheadle, getString(R.string.don_cheadle)),
-            ArtistInfo(R.drawable.paul_rudd, getString(R.string.paul_rudd)),
-            ArtistInfo(R.drawable.brie_larson, getString(R.string.brie_larson))
-        )
+    private fun fillList(): List<Actor> {
+        return movie.actors
+    }
+
+    companion object {
+
+        private const val MOVIE = "movie"
+
+        fun newInstance(movie: Movie) = FragmentMoviesDetails().apply {
+            arguments = Bundle().apply {
+                putParcelable(MOVIE, movie)
+            }
+        }
     }
 }
