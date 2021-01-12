5d3c03fa9b1c20fa0debe53b8471bbe0c5858d5e
Akhmadeev Timur <tmakhmadeev@gmail.com>
2021-01-12 21:59:27 +0300

    Changing the showMovieDetails function to work with the Movie class

--- app/src/main/java/ru/androidacademy/gooseeye/MainActivity.kt
+++ app/src/main/java/ru/androidacademy/gooseeye/MainActivity.kt
@@ -3,9 +3,9 @@ package ru.androidacademy.gooseeye
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
+import ru.androidacademy.gooseeye.data.Movie
 import ru.androidacademy.gooseeye.fragments.FragmentMoviesDetails
 import ru.androidacademy.gooseeye.fragments.FragmentMoviesList
-import ru.androidacademy.gooseeye.models.MovieInfo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +22,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun showMovieDetails(movieInfo: MovieInfo) {
-        val fragmentMoviesDetails = FragmentMoviesDetails()
+    fun showMovieDetails(movie: Movie) {
         supportFragmentManager.beginTransaction().apply {
-            add(R.id.fragment_container, fragmentMoviesDetails)
+            add(R.id.fragment_container, FragmentMoviesDetails.newInstance(movie))
             addToBackStack(null)
             setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             commit()
