1b11c6f492db8aa58455e156effe97c54bad36a2
Akhmadeev Timur <tmakhmadeev@gmail.com>
2021-02-10 15:49:36 +0300

    Adding @SerialName annotation for all fields

--- app/src/main/java/ru/androidacademy/gooseeye/data/Repository.kt
+++ app/src/main/java/ru/androidacademy/gooseeye/data/Repository.kt
@@ -12,11 +12,18 @@ class Repository() {
     private val jsonFormat = Json { ignoreUnknownKeys = true }
 
     @Serializable
-    private class JsonGenre(val id: Int, val name: String)
+    private class JsonGenre(
+        @SerialName("id")
+        val id: Int,
+        @SerialName("name")
+        val name: String
+    )
 
     @Serializable
     private class JsonActor(
+        @SerialName("id")
         val id: Int,
+        @SerialName("name")
         val name: String,
         @SerialName("profile_path")
         val profilePicture: String
@@ -24,21 +31,27 @@ class Repository() {
 
     @Serializable
     private class JsonMovie(
+        @SerialName("id")
         val id: Int,
+        @SerialName("title")
         val title: String,
         @SerialName("poster_path")
         val posterPicture: String,
         @SerialName("backdrop_path")
         val backdropPicture: String,
+        @SerialName("runtime")
         val runtime: Int,
         @SerialName("genre_ids")
         val genreIds: List<Int>,
+        @SerialName("actors")
         val actors: List<Int>,
         @SerialName("vote_average")
         val ratings: Float,
         @SerialName("vote_count")
         val votesCount: Int,
+        @SerialName("overview")
         val overview: String,
+        @SerialName("adult")
         val adult: Boolean
     )
 
