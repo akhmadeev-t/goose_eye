df867ff5efe91ed1fdf466a582f7ce24873c6d2b
Akhmadeev Timur <tmakhmadeev@gmail.com>
2021-02-18 17:17:47 +0300

    Adding serializable annotation and serialnames to Genre data class

--- app/src/main/java/ru/androidacademy/gooseeye/data/Genre.kt
+++ app/src/main/java/ru/androidacademy/gooseeye/data/Genre.kt
@@ -2,6 +2,14 @@ package ru.androidacademy.gooseeye.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Parcelize
-data class Genre(val id: Int, val name: String) : Parcelable
+@Serializable
+data class Genre(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("name")
+    val name: String
+) : Parcelable
