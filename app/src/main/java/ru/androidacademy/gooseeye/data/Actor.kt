94ea6e181b003b67066dd413c3ef5208e9a120bd
Akhmadeev Timur <tmakhmadeev@gmail.com>
2021-02-18 17:36:28 +0300

    Adding serializable annotation and serialnames to Actor data class

--- app/src/main/java/ru/androidacademy/gooseeye/data/Actor.kt
+++ app/src/main/java/ru/androidacademy/gooseeye/data/Actor.kt
@@ -2,10 +2,16 @@ package ru.androidacademy.gooseeye.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Parcelize
+@Serializable
 data class Actor(
+    @SerialName("id")
     val id: Int,
+    @SerialName("name")
     val name: String,
+    @SerialName("profile_path")
     val picture: String
 ) : Parcelable
